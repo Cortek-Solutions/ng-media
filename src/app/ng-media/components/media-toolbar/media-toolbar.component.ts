5a047826068cd0503783a8e202a9b1357acd68ef
anonymouse <anonymouse>
2018-02-17 21:15:55 +0330

    Refactor the markup

--- src/app/ng-media/components/media-toolbar/media-toolbar.component.ts
+++ src/app/ng-media/components/media-toolbar/media-toolbar.component.ts
@@ -7,7 +7,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MediaToolbarComponent implements OnInit {
 
-  constructor() { }
+  public types = [];
+  public dates = [];
+
+  constructor() {
+    this.types = [
+      {
+        key: 'all',
+        value: 'All'
+      },
+      {
+        key: 'image',
+        value: 'Images'
+      },
+      {
+        key: 'audio',
+        value: 'Audio'
+      },
+      {
+        key: 'video',
+        value: 'Video'
+      },
+      {
+        key: 'others',
+        value: 'Other types'
+      }
+    ];
+    this.dates = [
+      {
+        key: '2018-08',
+        value: 'Feb 2018'
+      }
+    ];
+  }
 
   ngOnInit() {
   }
