abd7162f5891c4cc08daab35b1b0d5cbcbb49923
Ali <connexion.founder@outlook.com>
2018-03-12 16:23:38 +0800

    Export the ng-media-component

--- public_api.ts
+++ public_api.ts
@@ -1,2 +1,3 @@
 export * from './src/app/ng-media/ng-media-module.module';
 export * from './src/app/ng-media/services/public.service';
+export * from './src/app/ng-media/components/ng-media/ng-media.component';
